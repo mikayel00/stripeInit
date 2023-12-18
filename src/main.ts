4fca202bb1edab808402aa7a983e36929b9679a5
Mikayel00 <mikayel.hovhannisyan27@gmail.com>
2023-12-18 22:55:10 +0400

    fix: add rawbody

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { rawBody: true });
 
   const configService = app.get(ConfigService);
 
